ad6ebd763638bdb9013d06b381d7fa8cf0de01d9
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 22:09:33 +0000

    Automatically backed up by Learn

--- code.rb
+++ code.rb
@@ -1,12 +1,7 @@
 
 
 def sort_array_char_count(array)
-  new_array = []
-  array.sort do |a, b|
-    a <=> b = new_value
-    new_array << new_value
-  end
-  puts new_array
+  array.sort {|a, b| b <=> a}
 end
 
 
