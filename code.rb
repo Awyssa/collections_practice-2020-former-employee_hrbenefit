8aa27845bcd9dd19b4a55278f39f7937932000b2
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 22:34:47 +0000

    Automatically backed up by Learn

--- code.rb
+++ code.rb
@@ -1,10 +1,11 @@
 
 
-def reverse_array(array)
-  puts "#{array.reverse}"
+def kesha_maker(array = ["Michael", "Dwight", "Creed"])
+  array.each do |word|
+    word[2] = $
 end
+
+kesha_maker()
 
 
 # sort_array_char_count(["Michael", "Dwight", "Creed"])
-
-reverse_array([1,2,3,4,5])
