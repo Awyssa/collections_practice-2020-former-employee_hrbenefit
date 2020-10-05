5afbd479c3185e88fee8b706a0f6543d42649e2e
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 22:50:10 +0000

    Done.

---
+++ code.rb
@@ -0,0 +1,10 @@
+
+
+def kesha_maker(array = ["Michael", "Dwight", "Creed"])
+  puts "#{array.each {|word| word[2] = "$"}}"
+end
+
+kesha_maker()
+
+
+# sort_array_char_count(["Michael", "Dwight", "Creed"])
--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array = ["Michael", "Dwight", "Creed"])
+  array.each {|word| word[2] = "$"}
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  array.map do |word|
+    if word == array[1]
+      word
+    else
+      word = word + "s"
+    end
+  end
+end
